7e6997bb81558c33614f97fb6220d6453458913d
evandrocoan <evandrocoan@hotmail.com>
2019-04-30 04:22:37 -0300

    Added copyright notice to makefile.mk

--- makefile.mk
+++ makefile.mk
@@ -1,5 +1,36 @@
 #!/usr/bin/make -f
 # https://stackoverflow.com/questions/7123241/makefile-as-an-executable-script-with-shebang
+#
+#   Copyright 2017-2019 @ Evandro Coan
+#   Helper functions and classes
+#
+#  Redistributions of source code must retain the above
+#  copyright notice, this list of conditions and the
+#  following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above
+#  copyright notice, this list of conditions and the following
+#  disclaimer in the documentation and/or other materials
+#  provided with the distribution.
+#
+#  Neither the name Evandro Coan nor the names of any
+#  contributors may be used to endorse or promote products
+#  derived from this software without specific prior written
+#  permission.
+#
+#  This program is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the
+#  Free Software Foundation; either version 3 of the License, or ( at
+#  your option ) any later version.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 ECHOCMD:=/bin/echo -e
 SHELL := /bin/bash
 
